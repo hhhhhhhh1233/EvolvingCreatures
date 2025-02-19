75e931edffd5347a14eb08d4c0c5816886c3bec3
hhhhhhhh <tagetjerngren@gmail.com>
2025-02-19 16:07:22 +0100

    Chore: Change some spacing and add function declarations for saving and loading creatures as a TODO

--- projects/EvolvingCreatures/code/Creature.h
+++ projects/EvolvingCreatures/code/Creature.h
@@ -12,17 +12,27 @@ public:
 	
 	Creature(physx::PxPhysics* Physics, physx::PxMaterial* PhysicsMaterial, physx::PxShapeFlags ShapeFlags, GraphicsNode Node, vec3 Scale);
 	~Creature();
+
 	CreaturePart* GetChildlessPart() const;
 	CreaturePart* GetRandomPart();
 	std::vector<CreaturePart*> GetAllParts();
 	std::vector<CreaturePart*> GetAllPartsFrom(CreaturePart* Part);
+
 	void AddRandomPart(physx::PxPhysics* Physics, physx::PxMaterial* PhysicsMaterial, physx::PxShapeFlags ShapeFlags, GraphicsNode Node);
 	void SetPosition(vec3 Position);
+
 	void AddToScene(physx::PxScene* Scene);
 	void RemoveFromScene(physx::PxScene* Scene);
+
 	void Update();
 	void Activate(float Force);
 	void Draw(mat4 ViewProjection);
+
 	void EnableGravity(bool NewState);
+
 	Creature* GetMutatedCreature(physx::PxPhysics* Physics);
 };
+
+/// TODO: Implement these features so that interesting creatures can be saved for later
+Creature* LoadCreatureFromFile(std::string FileName);
+void SaveCreatureToFile(Creature* CreatureToSave, std::string FileName);
