33a5d507f02007bba6bd039df67e2c89d5d62194
hhhhhhhh <tagetjerngren@gmail.com>
2025-02-19 16:06:20 +0100

    Fix: RandomFloatInRange won't assert if it generates Min or Max

--- projects/EvolvingCreatures/code/RandomUtils.h
+++ projects/EvolvingCreatures/code/RandomUtils.h
@@ -11,6 +11,6 @@ inline static float RandomFloat(float Mult = 1)
 inline static float RandomFloatInRange(float Min, float Max)
 {
 	float x = RandomFloat(Max - Min) - -Min;
-	assert(x < Max && x > Min);
+	assert(x <= Max && x >= Min);
 	return x;
 }
