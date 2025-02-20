6c708526015ddce553862f216de34d288887c71a
hhhhhhhh <tagetjerngren@gmail.com>
2025-02-20 13:01:58 +0100

    Feat: Add new random functions for integers, also rewrote RandomInRange to express the double negation as an addition

--- projects/EvolvingCreatures/code/RandomUtils.h
+++ projects/EvolvingCreatures/code/RandomUtils.h
@@ -8,9 +8,21 @@ inline static float RandomFloat(float Mult = 1)
 	return Mult * (float(rand()) / float(RAND_MAX));
 }
 
+/// Inclusive, will potentially return Min or Max
 inline static float RandomFloatInRange(float Min, float Max)
 {
-	float x = RandomFloat(Max - Min) - -Min;
+	float x = RandomFloat(Max - Min) + Min;
 	assert(x <= Max && x >= Min);
 	return x;
+}
+
+///  Exclusive, will never return Max
+inline static int RandomInt(int Max)
+{
+	return rand() % Max;
+}
+
+inline static int RandomIntInRange(int Min, int Max)
+{
+	return RandomInt(Max - Min) + Min;
 }
